6443758a051cf4c40c3486f71bf877e95d8d7937
EC2 Default User <ec2-user@ip-172-31-41-137.eu-north-1.compute.internal>
2025-05-19 14:31:55 +0000

    ログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   devise_for :users
-  #devise_for :users
   resources :prices, only: [:new, :index, :show]
   resources :items, only: [:new, :index, :show, :edit]
   resources :users, only: [:new, :show, :edit]
